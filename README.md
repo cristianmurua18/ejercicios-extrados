86b38ed1f5717723e6bb11c7006c921ed0fcf03f
Christian Murua <cristianmurua.18@gmail.com>
2024-11-07 13:20:08 -0300

    Agregar readme

---
+++ README.md
@@ -0,0 +1,8 @@
+﻿
+TAREA: REALIZAR EL EJERCICIO 1.
+
+Crear un programa que me indique, de que forma, poner 8 reinas en un tablero de ajedrez, de tal forma que ninguna reina pueda comerse a otra.
+
+Subir la solucion a github, enviar el link con el resultado.
+
+TRABAJARE CON C# y .NET 6
--- obj/Debug/net6.0/Ejercicio1.AssemblyInfo.cs
+++ obj/Debug/net6.0/Ejercicio1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3fe67bdfbb5c01226aaede6d2c9cc577022c789")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercicio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
